add8258fc64a494bddf831af67dd356358b222ab
abdul-rahman <smdarahman55@gmail.com>
2024-07-29 15:44:45 +0530

    DOM manipulation using id-reference

---
+++ 07_ID ref/index.js
@@ -0,0 +1,5 @@
+function bodyLoad() {
+    document.getElementById("pic").src = "../../DOM Hierarchy.png";
+    document.getElementById("btnRegister").value = "Register";
+    document.getElementById("btnLogin").value = "Login";
+}
