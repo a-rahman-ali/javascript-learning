5b387dc25a60774636f34e1f5edfde1202fa5e2e
abdul-rahman <smdarahman55@gmail.com>
2024-07-30 10:21:48 +0530

    DOM manipulation using CSS selectors ref

---
+++ 08_CSS Selectors ref/index.js
@@ -0,0 +1,5 @@
+function bodyLoad() {
+    document.querySelector("img").src = "../../DOM Hierarchy.png";
+    document.querySelector("#btnRegister").value = "Register";
+    document.querySelector(".btnLogin").value = "Login";
+}
