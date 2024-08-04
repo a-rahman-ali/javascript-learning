2110c951929334bd763242addb712f053814819e
abdul-rahman <smdarahman55@gmail.com>
2024-08-04 11:53:15 +0530

    js array sort()

---
+++ 15_DataTypes Non-Primitives/27_sorts.js
@@ -0,0 +1,6 @@
+var cities = ["Delhi", "Bangalore", "Chennai", "Hyd", "Mumbai", "Pune"];
+
+document.write(`given - ${cities.toString()} <br>`);
+document.write(`reverse - ${cities.reverse()} <br>`);
+document.write(`sort - ${cities.sort()} <br>`);
+document.write(`sort+reverse - ${cities.sort().reverse()} <br>`);
