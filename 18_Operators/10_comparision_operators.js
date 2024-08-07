8a2f32ab691971171f0905b9b37df70ff4320bb1
abdul-rahman <smdarahman55@gmail.com>
2024-08-07 10:10:43 +0530

    comparision operators

---
+++ 18_Operators/10_comparision_operators.js
@@ -0,0 +1,19 @@
+function loginClick() {
+    var username = document.getElementById("txtName").value;
+    var password = document.getElementById("txtPwd").value;
+
+    fetch("07_users.json")
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (users) {
+            for (var user of users) {
+                if (user.UserName === username) {
+                    document.getElementById("msg").innerHTML = "Login Success";
+                    // document.write("Login Success");
+                } else {
+                    document.getElementById("msg").innerHTML = "Invalid Username";
+                }
+            }
+        })
+}
