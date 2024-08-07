e24b4525ce2bea2035537533aa00acc8acf02d73
abdul-rahman <smdarahman55@gmail.com>
2024-08-07 09:27:29 +0530

    == vs === operator

---
+++ 18_Operators/06_comp_operators.js
@@ -0,0 +1,7 @@
+var x = 10;
+var y = "10";
+
+document.write(`x =  ${x} <br>y = ${y} <br>`);
+document.write(`typeof x =  ${typeof x} <br>typeof y = ${typeof y} <br>`);
+document.write(`x == y ? ${x == y} <br>`);
+document.write(`x === y ? ${x === y} <br>`);
