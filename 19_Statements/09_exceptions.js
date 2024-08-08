d14e5d1429d6c3a7c0b731a32c694057ca9a1f92
abdul-rahman <smdarahman55@gmail.com>
2024-08-08 13:22:12 +0530

    js exceptions

---
+++ 19_Statements/09_exceptions.js
@@ -0,0 +1,6 @@
+var x = parseInt(prompt("Enter the value of x : "));
+var y = parseInt(prompt("Enter the value of y : "));
+document.write("x = " + x + "<br>");
+document.write("y = " + y + "<br>");
+var z = x / y;
+document.write("Division result  = " + z + "<br>");
