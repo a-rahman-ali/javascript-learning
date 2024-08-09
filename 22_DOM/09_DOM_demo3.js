91858c135ace9137b769d7ae57e6bc9b2a7859e9
abdul-rahman <smdarahman55@gmail.com>
2024-08-09 23:32:43 +0530

    DOM id-reference example

---
+++ 22_DOM/09_DOM_demo3.js
@@ -0,0 +1,6 @@
+function bodyLoad() {
+    document.getElementById("pic").src = "images/thumbnails/thumbnail-1.webp";
+    document.getElementById("btnRegister").value = "Register";
+    document.getElementById("btnLogin").value = "Login";
+    document.getElementById("txtEmail").value = "johndoe@gmail.com";
+}
