2387e6bb6772181091ce4d793eae7122d7b8e9cd
abdul-rahman <smdarahman55@gmail.com>
2024-08-09 09:57:51 +0530

    js OOPs

---
+++ 21_OOPs/class.js
@@ -0,0 +1,19 @@
+// Class Declaration
+class Product {
+
+}
+
+// Class Expression
+var Employee = class {
+
+}
+
+var Prototype = class {
+    Name = "";
+    Price = 0;
+}
+Prototype = class {
+    EmpName = "";
+    Designation = "";
+}
+
