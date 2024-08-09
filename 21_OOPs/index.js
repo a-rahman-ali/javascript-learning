d4ab037a7bea43ce8625f76d37d6ce0276578678
abdul-rahman <smdarahman55@gmail.com>
2024-08-09 10:54:40 +0530

    js modules

---
+++ 21_OOPs/index.js
@@ -0,0 +1,2 @@
+import { Hello, Add } from "../21_OOPS/library/DemoModule.js";
+document.querySelector("p").innerHTML = Hello() + "<br>" + "Addition = " + Add(20, 30);
---
+++ 21_OOPs/library/DemoModule.js
@@ -0,0 +1,9 @@
+var msg = "Hello ! Welcome to JavaScript OOP";
+
+export function Hello() {
+    return msg;
+}
+
+export function Add(a, b) {
+    return a + b;
+}
