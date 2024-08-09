e915f797394562793b87eca74a8d8e4c1170ef55
abdul-rahman <smdarahman55@gmail.com>
2024-08-09 17:42:39 +0530

    DOM hierarchy example

---
+++ 22_DOM/07_DOM_demo.js
@@ -0,0 +1,6 @@
+function bodyLoad() {
+    window.document.images[0].src = "images/thumbnails/thumbnail-4.webp";
+    window.document.forms[0].elements[1].value = "Register";
+    window.document.forms[1].elements[0].value = "john@gmail.com";
+    window.document.forms[1].elements[1].value = "Login";
+}
