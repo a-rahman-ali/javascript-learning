f603f3c662ac4802dea1b0e91ade9746c34c8e72
abdul-rahman <smdarahman55@gmail.com>
2024-08-09 17:45:22 +0530

    DOM name-reference example

---
+++ 22_DOM/08_DOM_demo2.js
@@ -0,0 +1,7 @@
+function bodyLoad() {
+    pic.src = "images/thumbnails/thumbnail-4.webp";
+    frmRegister.btnRegister.value = "Register";
+    // btnRegister.value = "Register";
+    frmLogin.txtEmail.value = "john@gmail.com";
+    frmLogin.btnLogin.value = "Login";
+}
